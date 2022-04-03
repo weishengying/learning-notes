b96c45c15d7d48387928753bf729aed94d54dab8
weishengying <1343838695@qq.com>
2022-04-03 08:27:19 +0000

    update

---
+++ mlir/project/build/IR/CMakeFiles/MLIRinfrt_opsAttributesIncGen.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MLIRinfrt_opsAttributesIncGen"
+  "infrt_base.cpp.inc"
+  "infrt_base.h.inc"
+  "infrt_baseDialect.cpp.inc"
+  "infrt_baseDialect.h.inc"
+  "infrt_baseTypes.cpp.inc"
+  "infrt_baseTypes.h.inc"
+  "infrt_opsAttributes.cpp.inc"
+  "infrt_opsAttributes.h.inc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/MLIRinfrt_opsAttributesIncGen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ mlir/project/build/_deps/external_llvm-subbuild/CMakeFiles/external_llvm-populate.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "/weishengying/learning-notes/mlir/project/third-party/llvm/src/external_llvm-populate-stamp/external_llvm-populate-build"
+  "/weishengying/learning-notes/mlir/project/third-party/llvm/src/external_llvm-populate-stamp/external_llvm-populate-configure"
+  "/weishengying/learning-notes/mlir/project/third-party/llvm/src/external_llvm-populate-stamp/external_llvm-populate-download"
+  "/weishengying/learning-notes/mlir/project/third-party/llvm/src/external_llvm-populate-stamp/external_llvm-populate-install"
+  "/weishengying/learning-notes/mlir/project/third-party/llvm/src/external_llvm-populate-stamp/external_llvm-populate-mkdir"
+  "/weishengying/learning-notes/mlir/project/third-party/llvm/src/external_llvm-populate-stamp/external_llvm-populate-patch"
+  "/weishengying/learning-notes/mlir/project/third-party/llvm/src/external_llvm-populate-stamp/external_llvm-populate-test"
+  "/weishengying/learning-notes/mlir/project/third-party/llvm/src/external_llvm-populate-stamp/external_llvm-populate-update"
+  "CMakeFiles/external_llvm-populate"
+  "CMakeFiles/external_llvm-populate-complete"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/external_llvm-populate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
